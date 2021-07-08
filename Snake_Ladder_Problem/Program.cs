68fca6b5ae4ae95a796b78fe311a3543c9849da4
Neha121995 <jagtapneha1212@gmail.com>
2021-07-08 23:46:52 +0530

    Added : Welcome to Snake Ladder Game

---
+++ Snake_Ladder_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_Ladder_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the sneak ladder Game");
+            int Player_1 = 0;
+            Console.WriteLine("Player 1 Position is " + Player_1);
+        }
+    }
+}
